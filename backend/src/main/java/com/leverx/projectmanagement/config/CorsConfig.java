71c4bf5dd689ea670e392f156316d3870ff70f98
Mukhammad Pardaboev <mukhammadpardaboev@mail.com>
2025-09-17 16:43:07 +0500

    Fix cors

---
+++ backend/src/main/java/com/leverx/projectmanagement/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.leverx.projectmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Content-Type")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
